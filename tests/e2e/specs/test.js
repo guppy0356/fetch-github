4a6af9cab61ad8ac216c7a2d20bc305aa58f1a17
akira noguchi <guppy0356.nakira@gmail.com>
2019-03-25 02:25:54 +0900

    Add e2e test cases

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,36 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
+describe('fetching github user info', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route('GET', 'https://api.github.com/users/*', 'fixture:user.json').as('getInfo');
+  });
+
+  it('shows Github user info', () => {
+    cy.visit('/');
+    cy.get('input').type('guppy0356');
+    cy.get('button.success').click();
+    cy.wait('@getInfo');
+    cy.get('div.layout').contains('guppy0356');
+  });
+});
+
+describe('fetching failure', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'https://api.github.com/users/*',
+      status: 404,
+      response: []
+    }).as('getInfoFailure');
+  });
+
+  it('shows error message', () => {
+    cy.visit('/');
+    cy.get('input').type('guppy0356');
+    cy.get('button.success').click();
+    cy.wait('@getInfoFailure');
+    cy.contains('Oops! Github username not found.');
+  });
+});
